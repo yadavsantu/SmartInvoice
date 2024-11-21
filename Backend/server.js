62cb4721006cce659b87c5b4838a606f21dc4eef
sandeshprasai <sandeshprasai4321@gmail.com>
2024-11-21 16:55:32 +0545

    Managed Client and server in same repo

---
+++ Backend/server.js
@@ -0,0 +1,4 @@
+app.get("/api/data", (req, res) => {
+    res.json({ message: "Hello from the backend!" });
+  });
+  
---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import API from "./services/api";
+
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  async created() {
+    const response = await API.get("/api/data");
+    this.message = response.data.message;
+  },
+};
+</script>
