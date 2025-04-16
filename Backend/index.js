af3904423e9fc11cf5a3d593b8f7361086ef61e1
Sandesh Prasai <105264351+sandeshprasai@users.noreply.github.com>
2025-04-16 20:42:42 +0545

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ const morganFormat = ":method :url :status :response-time ms";
 
 app.use(
   cors({
-    origin: ["https://smart-invoice-7ws3.vercel.app"],
+    origin: ["https://smart-invoice-bice.vercel.app"],
     credentials: true,
   })
 );
